8b97aa9c8ff0c6d26a59499e305fd4c369084e3d
jayliang701 <156070304@qq.com>
2019-01-27 15:57:32 +0800

    update filter testing

--- src/App.js
+++ src/App.js
@@ -21,26 +21,44 @@ class RootApp extends React.Component {
     onSearch(filter) {
         this.setState({
             filter,
-            log: `${JSON.stringify(filter, null, 2)}`,
-
         });
     }
 
     render() {
-        const { log, filter } = this.state;
+        const { filter } = this.state;
         return (
             <div className="root" style={{ padding: 24 }}>
                 <SearchTool onSearch={this.onSearch.bind(this)} filter={filter} />
                 <div style={{ margin: '0 auto', marginTop: 30, width: 1190 }}>
                     <label>搜索过滤参数:</label>
                     {/* <textarea style={{ width:'100%', whiteSpace:'normal', wordWrap:'break-word', padding:'12px 16px', minHeight:280 }} value={log} onChange={evt => this.setState({ log: evt.currentTarget.value })} ></textarea> */}
-                    <table style={{ width:'100%', background:'#fff', border:'1px solid #ccc' }}>
+                    <table style={{ width:'100%', background:'#fff' }}>
                         {
                             Object.keys(filter).map(key => {
+                                const s = (filter[key] || []).map(item => {
+                                    if (item.options && item.options.length > 0) {
+                                        let opts = item.options.map(opt => opt.name);
+                                        return `${item.name} - ${opts.join(', ')}`;
+                                    }
+                                    return item.name;
+                                });
                                 return (
                                     <tr style={{ height: 28, lineHeight:'28px' }}>
-                                        <td style={{ padding: '0 8px', borderRight: '1px solid #ccc' }}>{key}</td>
-                                        <td style={{ padding: '0 8px' }}><textarea style={{ width: '100%', height: '100%', padding:'13px', display:'block', border:'none', resize: 'none' }} defaultValue={JSON.stringify(filter[key], null, 2)}></textarea></td>
+                                        <td style={{ padding: '0 8px' }}>{key}</td>
+                                        <td style={{ padding: '0 8px' }}>
+                                            <div 
+                                                style={{ width: '100%', height: '100%', padding:'6px', display:'block', border:'none', resize: 'none' }} 
+                                                >
+                                                {JSON.stringify(s, null, 2)}
+                                                <div style={{ position:'absolute', right: 12, top: 6, color: 'red', cursor: 'pointer' }}
+                                                    onClick={ ()=> {
+                                                        delete filter[key];
+                                                        this.setState({ filter });
+                                                    } }>
+                                                    Delete
+                                                </div>
+                                            </div>
+                                        </td>
                                     </tr>
                                 );
                             })
